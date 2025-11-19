4d96fbe5f2dcf410b90637eeed023e82442791b8
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-19 16:37:40 +0100

    add API-key

---
+++ src/api/config.js
@@ -0,0 +1,1 @@
+export const API_KEY = import.meta.env.VITE_MOVIE_DB_API_KEY
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,41 @@
+import { useGlobal } from "../contexts/GlobalContext"
+import { useEffect } from "react"
+import axios from "axios"
+import { API_KEY } from "../api/config"
+
 export default function Products() {
+
+    const {movies, setMovies} = useGlobal()
+
+    function fetchMovies() {
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=Avengers`)
+        .then((res) => setMovies(res.data.results))
+        .catch((err) => console.error('Errore:', err))
+    }
+
+    useEffect(fetchMovies, [])
 
     return (
         <>
-          <h1>Products</h1>
+          <div className="container">
+            <div>
+                <h1>products</h1>
+            </div>
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row.cols-lg-4">
+                {
+                    movies.map((thisMovie) => (
+                        <div key={thisMovie.id} className="col">
+                            <div className="card">
+
+                                <div className="card-body">
+                                    <h5>{thisMovie.original_title}</h5>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+          </div>
         </>
     )
 }
