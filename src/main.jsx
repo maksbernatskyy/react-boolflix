7ff608b6f76e92c9128fdaef5353cf800747e413
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-19 15:03:19 +0100

    import GlobalProvider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { GlobalProvider } from './contexts/GlobalContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   
-  <App />
+  <GlobalProvider>
+    <App />
+  </GlobalProvider>
 )
