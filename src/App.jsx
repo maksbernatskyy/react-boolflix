4c2766ba652b6a3d36f06300e39929edadcdf3b8
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-19 15:16:24 +0100

    add page HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import DefaultLayout from "./components/DefaultLayout"
+import HomePage from "./pages/HomePage"
 
 function App() {
   
@@ -8,7 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route element={<DefaultLayout />}>
-
+            <Route index element={<HomePage />} />
           </Route>
         </Routes>
       </BrowserRouter>
