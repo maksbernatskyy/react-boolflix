d4d193b106725e9c7e637387d766e504bcbf1d17
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-20 09:59:49 +0100

    add button send and the prevent default to the form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,20 @@
 export default function Form() {
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
     return (
         <>
          <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input 
                     type="text"
                     placeholder="Search" 
                 />
+                <button type="submit" className="btn btn-outline-danger mx-2">
+                    Send
+                </button>
             </form>
          </div>
         </>
