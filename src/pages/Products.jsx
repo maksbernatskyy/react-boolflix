c85b5cac0429d73d9a59d59e5791e2a0dc0357bb
maksbernatskyy <maksbernatskyy@gmail.com>
2025-11-20 11:29:15 +0100

    Now you can search on the form

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -6,14 +6,15 @@ import { API_KEY } from "../api/config"
 export default function Products() {
 
     const {movies, setMovies} = useGlobal()
+    const {search} = useGlobal()
 
     function fetchMovies() {
-        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=matrix`)
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${search}`)
         .then((res) => setMovies(res.data.results))
         .catch((err) => console.error('Errore:', err))
     }
 
-    useEffect(fetchMovies, [])
+    useEffect(fetchMovies, [search])
 
     return (
         <>
